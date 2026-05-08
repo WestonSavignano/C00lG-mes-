a535bdf3ab04f1310a10fa9d70acaf7f1a725e46
PaulSavignano <paul.savignano@gmail.com>
2026-05-08 11:34:06 -0700

    chore: deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
+const base = process.env.GITHUB_PAGES === 'true' ? '/C00lG-mes-/' : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [react()],
   test: {
     environment: 'jsdom',
