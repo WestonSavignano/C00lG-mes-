f1d5a7bc64964d26f43f2f387f426c99e66575ef
Paul Savignano <Paul.Savignano@gmail.com>
2026-09-10 19:36:45 -0700

    refactor: make donut run page a runtime adapter

--- src/games/donut-run/DonutRunPage.tsx
+++ src/games/donut-run/DonutRunPage.tsx
@@ -1,17 +1,7 @@
-import H1 from '../../components/H1'
-import Page from '../../components/Page'
-import PageHeader from '../../components/PageHeader'
 import DonutRun from './DonutRun'
 
 function DonutRunPage() {
-  return (
-    <Page className="game-page">
-      <PageHeader>
-        <H1>Donut Run</H1>
-      </PageHeader>
-      <DonutRun />
-    </Page>
-  )
+  return <DonutRun />
 }
 
 export default DonutRunPage
