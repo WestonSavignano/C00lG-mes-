5f0f3271934d24a8b4d0a6f5a8f55bb27eb4953f
Paul Savignano <Paul.Savignano@gmail.com>
2026-09-10 19:36:51 -0700

    refactor: make neon drift page a runtime adapter

--- src/games/neon-drift/NeonDriftPage.tsx
+++ src/games/neon-drift/NeonDriftPage.tsx
@@ -1,21 +1,7 @@
-import BackLink from '../../components/BackLink'
-import H1 from '../../components/H1'
-import P from '../../components/P'
-import Page from '../../components/Page'
-import PageHeader from '../../components/PageHeader'
 import NeonDriftGame from './NeonDriftGame'
 
 function NeonDriftPage() {
-  return (
-    <Page className="game-page">
-      <PageHeader>
-        <BackLink to="/games">Games</BackLink>
-        <H1>Neon Drift</H1>
-        <P>Outrun the swarm, collect energy cores, and deploy turrets to survive the neon arena.</P>
-      </PageHeader>
-      <NeonDriftGame />
-    </Page>
-  )
+  return <NeonDriftGame />
 }
 
 export default NeonDriftPage
