b80476c8065029d0bd28a1ccdafc71d45c023854
Paul Savignano <Paul.Savignano@gmail.com>
2026-09-10 18:12:30 -0700

    feat: route games through catalog

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import HeaderNav from './components/HeaderNav'
-import BitPlanesPage from './games/bit-planes/BitPlanesPage'
-import DonutRunPage from './games/donut-run/DonutRunPage'
-import NeonDriftPage from './games/neon-drift/NeonDriftPage'
-import WarriorPage from './games/warrior/WarriorPage'
-import Warrior2Page from './games/warrior2/WarriorPage'
-import WormBattlesPage from './games/worm-battles/WormBattlesPage'
+import { gameRouteEntries } from './games/catalog/gameRoutes'
 import ChatPage from './pages/ChatPage'
 import GamesPage from './pages/GamesPage'
 import HomePage from './pages/HomePage'
@@ -22,12 +17,9 @@ function App() {
         <Route path="/soundboard" element={<Navigate replace to="/chat" />} />
         <Route path="/soundboard/sound" element={<Navigate replace to="/chat" />} />
         <Route path="/games" element={<GamesPage />} />
-        <Route path="/games/plane-blaster" element={<BitPlanesPage />} />
-        <Route path="/games/donut-run" element={<DonutRunPage />} />
-        <Route path="/games/neon-drift" element={<NeonDriftPage />} />
-        <Route path="/games/worm-battles" element={<WormBattlesPage />} />
-        <Route path="/games/warrior" element={<WarriorPage />} />
-        <Route path="/games/warrior2" element={<Warrior2Page />} />
+        {gameRouteEntries.map((entry) => (
+          <Route key={entry.game.id} path={entry.path} element={entry.element} />
+        ))}
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
