58336f3332220b6c98cdafc7c2c13beaec7d674b
Paul Savignano <Paul.Savignano@gmail.com>
2026-09-10 19:37:11 -0700

    refactor: make warrior2 page a runtime adapter

--- src/games/warrior2/WarriorPage.tsx
+++ src/games/warrior2/WarriorPage.tsx
@@ -1,17 +1,7 @@
-import H1 from '../../components/H1'
-import Page from '../../components/Page'
-import PageHeader from '../../components/PageHeader'
 import Warrior from './Warrior'
 
 function WarriorPage() {
-  return (
-    <Page className="game-page">
-      <PageHeader>
-        <H1>Warrior2</H1>
-      </PageHeader>
-      <Warrior />
-    </Page>
-  )
+  return <Warrior />
 }
 
 export default WarriorPage
